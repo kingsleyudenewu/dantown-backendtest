f65a92e778f0e687262eeb5fc8b3c62543571147
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-17 23:10:09 +0100

    Add enum status and create transasction type and status enums

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Trasnaction>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
  */
-class TrasnactionFactory extends Factory
+class TransactionFactory extends Factory
 {
     /**
      * Define the model's default state.
