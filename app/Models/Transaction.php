a18b3332e3a194a15a6cd2caf1a2017215dc3efd
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-17 23:26:37 +0100

    Add transaction dto file and add logic method to create transaction.

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function createUserTx(
+        User $user,
+        float $amount,
+        string $narration,
+        string $type,
+        bool $credit = false
+    ): Transaction {
+        $amount = ($credit ? 1 : -1) * $amount;
+
+        return $user->transactions()->create([
+            'amount' => $amount,
+            'type' => $type,
+            'reference' => strtoupper(uniqid('TX-').time()),
+            'ip_address' => request()->ip(),
+            'narration' => $narration,
+        ]);
+    }
 }
