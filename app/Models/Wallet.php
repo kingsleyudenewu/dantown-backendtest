8bb280f46acf490158e200fe40f544039e764f9a
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-17 23:26:55 +0100

    Add a method to update a wallet.

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -15,4 +15,14 @@ class Wallet extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public static function updateWalletBalance(float $amount, bool $credit = false)
+    {
+        $amount = ($credit ? 1 : -1) * $amount;
+
+        return tap(auth()->user()->wallet)->update([
+            'initial_amount' => auth()->user()->wallet->actual_amount,
+            'actual_amount' => auth()->user()->wallet->actual_amount + $amount,
+        ]);
+    }
 }
