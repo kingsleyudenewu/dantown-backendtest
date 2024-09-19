170bf639e007556bc741545286384ccc6f897f30
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-19 14:49:00 +0100

    Add new routes for trnasactions and add config files for the roles

---
+++ config/roles.php
@@ -0,0 +1,6 @@
+<?php
+
+return[
+    'maker' => 'Maker',
+    'checker' => 'Checker',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Maker can create transactions
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::get('/transactions/create', [TransactionController::class, 'create'])->name('transactions.create')->middleware('role:maker');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store')->middleware('role:maker');
+
+    // Checker can approve or reject transactions
+    Route::post('/transactions/{transaction}/approve', [TransactionController::class, 'approve'])->name('transactions.approve')->middleware('role:checker');
+    Route::post('/transactions/{transaction}/reject', [TransactionController::class, 'reject'])->name('transactions.reject')->middleware('role:checker');
 });
 
 require __DIR__ . '/auth.php';
